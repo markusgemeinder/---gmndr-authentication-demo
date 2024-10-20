d8a76b4a07e7a35e4e889024f2bc95d0bbd57a7f
markusgemeinder <190774@gmx.de>
2024-10-20 23:35:32 +0200

    Update AboutTechstack

--- app/components/About/AboutTechstack.js
+++ app/components/About/AboutTechstack.js
@@ -1,9 +1,13 @@
-// /app/components/About/AboutTechstack.js
-
 'use client';
 
 import ScrollToTop from '@/app/components/Common/ScrollToTop';
-import { AboutTitle, AboutParagraph } from '@/app/components/About/AboutStyles';
+import {
+  AboutTitle,
+  AboutParagraph,
+  AboutListContainer,
+  AboutList,
+  AboutHeadline,
+} from '@/app/components/About/AboutStyles';
 
 export default function AboutTechstack() {
   return (
@@ -11,10 +15,33 @@ export default function AboutTechstack() {
       <ScrollToTop />
       <AboutTitle>Techstack</AboutTitle>
       <AboutParagraph>Technologies and tools used in this project:</AboutParagraph>
-      <AboutParagraph>
-        ● Next.js ● React ● HTML ● CSS ● JavaScript ● GitHub ● Node.js ● Vercel ● useSWR ● react-hot-toast ● MongoDB ●
-        Cloudinary ● Figma ● Lottie Files ● Affinity Designer
-      </AboutParagraph>
+      <AboutHeadline>Frontend</AboutHeadline>
+      <AboutListContainer>
+        <AboutList>Next.js</AboutList>
+        <AboutList>React</AboutList>
+        <AboutList>HTML</AboutList>
+        <AboutList>CSS</AboutList>
+        <AboutList>JavaScript</AboutList>
+        <AboutList>react-icons/fa</AboutList>
+      </AboutListContainer>
+      <AboutHeadline>Backend</AboutHeadline>
+      <AboutParagraph>Authentication, APIs, mail services:</AboutParagraph>
+      <AboutListContainer>
+        <AboutList>Node.js</AboutList>
+        <AboutList>NextAuth</AboutList>
+        <AboutList>Nodemailer</AboutList>
+        <AboutList>NextResponse</AboutList>
+      </AboutListContainer>
+      <AboutHeadline>Database</AboutHeadline>
+      <AboutListContainer>
+        <AboutList>MongoDB</AboutList>
+      </AboutListContainer>
+      <AboutHeadline>Other Tools</AboutHeadline>
+      <AboutParagraph>Version control, deployment:</AboutParagraph>
+      <AboutListContainer>
+        <AboutList>GitHub</AboutList>
+        <AboutList>Vercel</AboutList>
+      </AboutListContainer>
     </>
   );
 }
