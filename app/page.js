a4f20ac6e735138ead5c27748f03f867432f9941
markusgemeinder <190774@gmx.de>
2024-10-30 10:26:47 +0100

    Hide button "Send Test Email" on Homepage

--- app/page.js
+++ app/page.js
@@ -12,61 +12,41 @@ export default function Home() {
   const [loading, setLoading] = useState(false);
   const [message, setMessage] = useState('');
 
-  const handleTestEmail = async () => {
-    setLoading(true);
-    setMessage('');
+  // const handleTestEmail = async () => {
+  //   setLoading(true);
+  //   setMessage('');
 
-    try {
-      const response = await fetch('/api/send-test-email', {
-        method: 'POST',
-      });
+  //   try {
+  //     const response = await fetch('/api/send-test-email', {
+  //       method: 'POST',
+  //     });
 
-      if (!response.ok) {
-        throw new Error('Failed to send test email.');
-      }
+  //     if (!response.ok) {
+  //       throw new Error('Failed to send test email.');
+  //     }
 
-      const data = await response.json();
-      setMessage(data.message);
-    } catch (error) {
-      setMessage('Failed to send test email.');
-    } finally {
-      setLoading(false);
-    }
-  };
+  //     const data = await response.json();
+  //     setMessage(data.message);
+  //   } catch (error) {
+  //     setMessage('Failed to send test email.');
+  //   } finally {
+  //     setLoading(false);
+  //   }
+  // };
 
   return (
     <Container>
       <ScrollToTop />
       <Title>#GMNDR Authentication Demo</Title>
       <SessionStatus />
-      <Button
+      {/* <Button
         onClick={handleTestEmail}
         bgColor='var(--color-button-delete)'
         hoverColor='var(--color-button-delete-hover)'
         disabled={loading}>
         {loading ? 'Sending...' : 'Send Test Email'}
-      </Button>
+      </Button> */}
       {message && <p>{message}</p>}
     </Container>
   );
 }
-
-// =============ORIGINAL
-
-// // /app/page.js
-
-// 'use client';
-
-// import SessionStatus from '@/app/components/Authentication/SessionStatus';
-// import { Container, Title } from '@/app/components/Common/CommonStyles';
-// import ScrollToTop from '@/app/components/Common/ScrollToTop';
-
-// export default function Home() {
-//   return (
-//     <Container>
-//       <ScrollToTop />
-//       <Title>#GMNDR Authentication Demo</Title>
-//       <SessionStatus />
-//     </Container>
-//   );
-// }
