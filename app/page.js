c31209ac37a1e9e70a64f77b44c1d329979dcb8e
markusgemeinder <190774@gmx.de>
2024-10-30 18:29:29 +0100

    Implement LanguageProvider to Navigation and Home

--- app/page.js
+++ app/page.js
@@ -2,50 +2,53 @@
 
 'use client';
 
-import { useState } from 'react';
+import { useState, useContext } from 'react';
 import SessionStatus from '@/app/components/Authentication/SessionStatus';
 import { Container, Title } from '@/app/components/Common/CommonStyles';
 import ScrollToTop from '@/app/components/Common/ScrollToTop';
 import Button from '@/app/components/Button/Button';
+import LanguageContext from '@/app/components/LanguageProvider';
+import { getText } from '@/lib/languageLibrary';
 
 export default function Home() {
   const [loading, setLoading] = useState(false);
   const [message, setMessage] = useState('');
+  const { language } = useContext(LanguageContext); // Zugriff auf die aktuelle Sprache
 
-  // const handleTestEmail = async () => {
-  //   setLoading(true);
-  //   setMessage('');
+  const handleTestEmail = async () => {
+    setLoading(true);
+    setMessage('');
 
-  //   try {
-  //     const response = await fetch('/api/send-test-email', {
-  //       method: 'POST',
-  //     });
+    try {
+      const response = await fetch('/api/send-test-email', {
+        method: 'POST',
+      });
 
-  //     if (!response.ok) {
-  //       throw new Error('Failed to send test email.');
-  //     }
+      if (!response.ok) {
+        throw new Error('Failed to send test email.');
+      }
 
-  //     const data = await response.json();
-  //     setMessage(data.message);
-  //   } catch (error) {
-  //     setMessage('Failed to send test email.');
-  //   } finally {
-  //     setLoading(false);
-  //   }
-  // };
+      const data = await response.json();
+      setMessage(data.message);
+    } catch (error) {
+      setMessage('Failed to send test email.');
+    } finally {
+      setLoading(false);
+    }
+  };
 
   return (
     <Container>
       <ScrollToTop />
-      <Title>#GMNDR Authentication Demo</Title>
+      <Title>{getText('home', 'title', language)}</Title> {/* Dynamischer Titel */}
       <SessionStatus />
-      {/* <Button
+      <Button
         onClick={handleTestEmail}
         bgColor='var(--color-button-delete)'
         hoverColor='var(--color-button-delete-hover)'
         disabled={loading}>
-        {loading ? 'Sending...' : 'Send Test Email'}
-      </Button> */}
+        {loading ? getText('home', 'button_sending', language) : getText('home', 'button_send_email', language)}
+      </Button>
       {message && <p>{message}</p>}
     </Container>
   );
