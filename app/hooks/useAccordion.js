e942215b59cca3d98ea0f83a564fed3944bb49ed
markusgemeinder <190774@gmx.de>
2024-11-04 09:34:07 +0100

    Adjust padding added in useAccordion hook

--- app/hooks/useAccordion.js
+++ app/hooks/useAccordion.js
@@ -5,6 +5,15 @@ const useAccordion = (padding = 0) => {
   const [contentHeight, setContentHeight] = useState(0);
   const contentRef = useRef(null);
 
+  const calculateTotalHeight = () => {
+    if (!contentRef.current) return 0;
+    const scrollHeight = contentRef.current.scrollHeight;
+
+    // Dynamische Anpassung des Paddings
+    const adjustedPadding = scrollHeight < 200 ? padding : padding * 1.4;
+    return scrollHeight + adjustedPadding;
+  };
+
   const toggleAccordion = () => {
     if (isOpen) {
       setIsOpen(false);
@@ -12,16 +21,14 @@ const useAccordion = (padding = 0) => {
         setContentHeight(0);
       }, 300);
     } else {
-      const totalHeight = contentRef.current.scrollHeight + padding;
-      setContentHeight(totalHeight);
+      setContentHeight(calculateTotalHeight());
       setIsOpen(true);
     }
   };
 
   useEffect(() => {
     if (isOpen && contentRef.current) {
-      const totalHeight = contentRef.current.scrollHeight + padding;
-      setContentHeight(totalHeight);
+      setContentHeight(calculateTotalHeight());
     }
   }, [isOpen, padding]);
 
