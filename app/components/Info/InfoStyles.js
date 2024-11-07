35783942382d3cdf64582dfe83e2c4da606f736f
markusgemeinder <190774@gmx.de>
2024-11-07 11:51:51 +0100

    Update InfoStyles.js

--- app/components/Info/InfoStyles.js
+++ app/components/Info/InfoStyles.js
@@ -95,26 +95,26 @@ export const InfoImageWithLink = styled(Image)`
 `;
 
 export const InfoImageWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
   position: fixed;
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
-  z-index: 1000;
   width: 90%;
   height: auto;
   max-width: 90vw;
   max-height: 90vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
+  border-radius: 8px;
+  box-shadow: 0 0 8px rgba(0, 0, 0, 0.5);
+  z-index: 1000;
 `;
 
 export const InfoImageFullSize = styled(Image)`
-  max-width: 90%;
-  max-height: 90%;
+  max-width: 90vw;
+  max-height: 90vh;
   object-fit: contain;
-  border-radius: 8px;
-  box-shadow: 0 0 8px rgba(0, 0, 0, 0.5);
 `;
 
 export const Overlay = styled.div`
