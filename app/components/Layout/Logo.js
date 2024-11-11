a11baccf2dc13a7f5faec6e51d49b273474c07f3
markusgemeinder <190774@gmx.de>
2024-11-11 17:08:16 +0100

    Refactor Navigation component, implement new #GMNDR demobits logo

---
+++ app/components/Layout/Logo.js
@@ -0,0 +1,23 @@
+// /app/components/Layout/Logo.js
+
+import styled from 'styled-components';
+import Image from 'next/image';
+
+const logoSrc = '/images/gmndr-demobits-logo.png?v=' + new Date().getTime();
+
+const LogoContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+  width: auto;
+  height: auto;
+`;
+
+export default function Logo() {
+  return (
+    <LogoContainer>
+      <Image src={logoSrc} alt='GMNDR Demo Logo' width={70} height={25} />
+    </LogoContainer>
+  );
+}
