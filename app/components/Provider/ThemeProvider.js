3a78fb58dfe596624158bbbb2d69fa823a4c3501
markusgemeinder <190774@gmx.de>
2025-01-02 11:27:53 +0100

    Debug: Fix hydration errors in LanguageProvider and ThemeProvider

--- app/components/Provider/ThemeProvider.js
+++ app/components/Provider/ThemeProvider.js
@@ -7,23 +7,29 @@ import { createContext, useState, useEffect } from 'react';
 export const ThemeContext = createContext();
 
 export default function ThemeProvider({ children }) {
-  const [theme, setTheme] = useState('light');
+  const [theme, setTheme] = useState(null);
 
   useEffect(() => {
     if (typeof window !== 'undefined') {
-      const storedTheme = localStorage.getItem('authDemo_theme') || 'light';
+      const storedTheme = localStorage.getItem('cpg_theme') || 'light';
       setTheme(storedTheme);
       document.documentElement.setAttribute('data-theme', storedTheme);
     }
   }, []);
 
   useEffect(() => {
-    localStorage.setItem('authDemo_theme', theme);
-    document.documentElement.setAttribute('data-theme', theme);
+    if (theme !== null) {
+      localStorage.setItem('cpg_theme', theme);
+      document.documentElement.setAttribute('data-theme', theme);
+    }
   }, [theme]);
 
   function toggleTheme() {
     setTheme((prevTheme) => (prevTheme === 'light' ? 'dark' : 'light'));
+  }
+
+  if (theme === null) {
+    return null; // Verhindert das Rendern vor der Hydration
   }
 
   return <ThemeContext.Provider value={{ theme, toggleTheme }}>{children}</ThemeContext.Provider>;
